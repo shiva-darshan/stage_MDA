0eda0e4411ad655953cd15bf286858823d1be69e
Shiva Darshan <sd2997@nyu.edu>
2022-09-14 15:21:07 +0200

    changes

--- fput_plot.jl
+++
@@ -1,8 +0,0 @@
-using Plots
-
-a = -1.6667
-b = .4444
-
-plot3b = plot(x -> x^2/2 + a*x^3/3 + b*x^4/4, -2, 5, ylims = (-2, 10), legend = false,
-    title = string("FPUT Potential with a = ", a, " and b = ", b))
-savefig(plot3b, "FPUT_pot_min3.png")
